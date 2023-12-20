bbbf6fec83883ae63cecefe54a92a68b2d3236df
Alex <alexstarochkin@gmail.com>
2023-12-20 12:45:50 +0700

    1

--- main.py
+++ main.py
@@ -34,7 +34,9 @@ def gen_analitica(file_2, file_1, user_id):
     df1_1 = pd.read_excel(file_1, sheet_name='Товары', skiprows=1)
 
 
-    code = df2['Код номенклатуры'].dropna().unique()
+    code = df2['Код номенклатуры'].unique()
+    if 0 in code:
+        np.delete(code, np.where(0))
     print(code)
     # articul = np.sort(df2['Артикул поставщика'].unique())
     articul_nan = (df2['Артикул поставщика'].dropna().unique())
